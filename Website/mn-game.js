4c28253ba0b0fef47ac037090fed8efb11bc130c
Neo_Tsang <neo@tsang.nu>
2022-12-20 15:50:00 +0100

    base game

---
+++ Website/mn-game.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("box");
+const ctx = canvas.getContext("2d");
+
+canvas.width    = 625;
+canvas.height   = 500;
+
+ctx.fillStyle = "lightblue";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
